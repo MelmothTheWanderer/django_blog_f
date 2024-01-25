de95828dd3f00d7d1e6d3fffeb6c852e114d224d
Jamie Simms <jamiesimms@protonmail.com>
2024-01-25 12:51:38 +0000

    added a secret key

--- codestar/codestar/settings.py
+++ codestar/codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i85u6nx!52%kb)fa$+zokoyjd-=l@4)%_frc9ywykln@wfca9%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- codestar/env.py
+++ codestar/env.py
@@ -1,3 +1,5 @@
 import os
 
 os.environ.setdefault("DATABASE_URL","postgres://jovjpxji:52XqVeLFCCRfDJCSlXbT7iUE9zZxZm8a@snuffleupagus.db.elephantsql.com/jovjpxji")
+
+os.environ.setdefault("SECRET_KEY", "theturkeytalks")
