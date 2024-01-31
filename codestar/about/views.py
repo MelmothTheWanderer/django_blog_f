0f8c4fe5b25689af18763ef24bf51051c3acea98
Jamie Simms <jamiesimms@protonmail.com>
2024-01-31 22:36:01 +0000

    added everything for a new app

--- codestar/about/views.py
+++ codestar/about/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import About
 
 # Create your views here.
+
+def about_detail(request):
+
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render (
+        request,
+        "about/about_details.html",
+        {"about" : about}
+    )
